82eb31c1388854591272926050a2d2bfd25c3c74
Štefan Baebler <stefan.baebler@gmail.com>
2024-05-25 08:17:48 +0200

    Initial EP2024 volitve scraping script

---
+++ README.md
@@ -0,0 +1,7 @@
+# DVK volitve EP2024
+
+[![Volitve EP2024 update](https://github.com/stefanb/dvk-volitve-ep2024/actions/workflows/update.yml/badge.svg)](https://github.com/stefanb/dvk-volitve-ep2024/actions/workflows/update.yml)
+
+Sources:
+
+* https://volitve.dvk-rs.si/ -> [volitve/](volitve/)
---
+++ update.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -e
+
+VolitveBASEURL="https://volitve.dvk-rs.si/ep2024"
+mkdir -p volitve
+
+curl -s "${VolitveBASEURL}/config/config.json"   | jq > volitve/config.json
+curl -s "${VolitveBASEURL}/data/obvestila.json"  | jq > volitve/obvestila.json
+curl -s "${VolitveBASEURL}/data/data.json"       | jq > volitve/data.json
+jq -r '(.slovenija.enote | map({st: .st, naziv: .naz} ))| (.[0] | to_entries | map(.key)), (.[] | [.[]]) | @csv' volitve/data.json > volitve/enote.csv
+curl -s "${VolitveBASEURL}/data/liste.json"      | jq > volitve/liste.json
+jq -r '(.[0] | to_entries | map(.key)), (.[] | [.[]]) | @csv' volitve/liste.json > volitve/liste.csv
+curl -s "${VolitveBASEURL}/data/kandidati.json"  | jq > volitve/kandidati.json
+jq -r 'map({zap_st: .zap_st, st: .st, id: .id, ime: .ime, priimek: .pri, datum_rojstva: .dat_roj[0:10], delo: .del , obcina: .obc , naselje: .nas , ulica: .ul , hisna_st: .hst , enota: .enota, okraj_1: .okraji[0], okraj_2: .okraji[1] }) | (.[0] | to_entries | map(.key)), (.[] | [.[]]) | @csv' volitve/kandidati.json > volitve/kandidati.csv
+curl -s "${VolitveBASEURL}/data/zgod_udel.json"  | jq > volitve/zgod_udel.json
+
+# Iz navodil medijem:
+# https://www.dvk-rs.si/volitve-in-referendumi/drzavni-zbor-rs/volitve-drzavnega-zbora-rs/volitve-v-dz-2022/#accordion-1731-body-6
+curl -s "${VolitveBASEURL}/data/udelezba.json"            | jq > volitve/udelezba.json
+curl -s "${VolitveBASEURL}/data/udelezba.csv"                  > volitve/udelezba.csv
+curl -s "${VolitveBASEURL}/data/rezultati.json"           | jq > volitve/rezultati.json
+curl -s "${VolitveBASEURL}/data/rezultati.csv"                 > volitve/rezultati.csv
+curl -s "${VolitveBASEURL}/data/kandidati_rezultati.json" | jq > volitve/kandidati_rezultati.json
+curl -s "${VolitveBASEURL}/data/mandati.csv"                   > volitve/mandati.csv
+
+
+for VE in {1..8}
+do
+    VETEMP="0${VE}"
+    VEPAD="${VETEMP: -2}" #pad left with 0s to max 2 chars
+    for VO in {1..11}
+    do
+        VOTEMP="0${VO}"
+        VOPAD="${VOTEMP: -2}"
+        echo "Scraping VE:${VEPAD} VO:${VOPAD}..."
+        curl -s "${VolitveBASEURL}/data/volisca_${VEPAD}_${VOPAD}.json" | jq > volitve/volisca_${VEPAD}_${VOPAD}.json
+    done
+done
